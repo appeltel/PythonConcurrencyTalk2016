2f0d1b6943ed8a6a7386ad5a8450d146c5fc0522
Eric Appelt <eric.appelt@gmail.com>
2016-08-27 12:28:56 -0500

    Add animals flask application

---
+++ resources/animals_api.py
@@ -0,0 +1,36 @@
+"""
+Simple flask application for the See 'n Say (TM) inspired
+animals API
+"""
+from time import sleep
+
+from flask import Flask
+app = Flask(__name__)
+
+FARM = {
+    'cow': 'Moo!',
+    'pig': 'Oink!',
+    'sheep': 'Baaa!',
+    'chicken': 'Cluck!'
+}
+
+
+@app.route('/animals/')
+def hello():
+    """
+    Welcome
+    """
+    msg = 'Welcome to the farm!'
+    return msg, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+
+@app.route('/animals/<animal>')
+def speak(animal):
+    """
+    What does this animal say???
+    """
+    if animal not in FARM:
+        return 'The animal {0} was not found.'.format(animal), 404
+
+    sleep(5)
+    return FARM[animal], 200, {'Content-Type': 'text/plain; charset=utf-8'}
