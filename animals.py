3ac56049c404cf8d1986d69dfff6f79aa6620d81
Eric Appelt <eric.appelt@gmail.com>
2016-08-26 20:05:44 -0500

    Simplify basic example
    Get rid of the list sorting in the basic
    talk example, just retrieve animals and
    animated print the sounds.

--- animals.py
+++ animals.py
@@ -3,21 +3,16 @@ import requests
 from radprint import radprint
 
 
-ANIMALS = ['cow', 'pig', 'chicken']
-
-
-def process_animal(animal, results):
+def speak(animal):
     """
     Retrieves the sound for the given animal,
-    prints it with animation, and then appends
-    the sound to the results list.
+    and prints it with animation.
     """
     response = requests.get(
         'https://ericappelt.com/animals/{0}'.format(animal)
     )
     sound = response.text
     radprint('The {0} says "{1}".'.format(animal, sound))
-    results.append(sound)
 
 
 def main():
@@ -25,11 +20,9 @@ def main():
     Process all animals and then print a sorted list of
     their sounds.
     """
-    results = []
-    for animal in ANIMALS:
-        process_animal(animal, results)
-    results.sort()
-    print('\nAnimal sounds: {0}'.format(results))
+    animals = ['cow', 'pig', 'chicken']
+    for animal in animals:
+        speak(animal)
 
 
 if __name__ == '__main__':
