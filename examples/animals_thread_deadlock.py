0adead5bf566f4d43e2e6221e954fa514e2a156a
Eric Appelt <eric.appelt@gmail.com>
2016-09-06 17:11:57 -0500

    Add deadlock and context manager examples

---
+++ examples/animals_thread_deadlock.py
@@ -0,0 +1,48 @@
+"""
+This module is a naive threaded version of
+animals.py in which the animated printing
+is synchronized between threads using a simple
+threading.Lock. A raised exception may cause a
+deadlock.
+"""
+from threading import Thread, Lock
+
+import requests
+
+from radprint import radprint
+
+
+def speak(animal, printlock):
+    """
+    Retrieves the sound for the given animal,
+    and prints it with animation.
+    """
+    response = requests.get(
+        'https://ericappelt.com/animals/{0}'.format(animal)
+    )
+    sound = response.text
+    printlock.acquire()
+    if 'Moo' in sound:
+        raise ValueError('Not happy about moo.')
+    radprint('The {0} says "{1}".'.format(animal, sound))
+    printlock.release()
+
+
+def main():
+    """
+    Retrieve and print sounds for all animals.
+    """
+    animals = ['cow', 'pig', 'chicken']
+    threads = []
+    printlock = Lock()
+    for animal in animals:
+        worker = Thread(target=speak, args=(animal, printlock))
+        threads.append(worker)
+        worker.start()
+
+    for worker in threads:
+        worker.join()
+
+
+if __name__ == '__main__':
+    main()
