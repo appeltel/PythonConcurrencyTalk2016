3ebcf1d8ed8fbe6ba93b7e14a6587d6dc8bc644a
Eric Appelt <eric.appelt@gmail.com>
2016-09-25 20:24:52 -0500

    Cleanup basic iterator example

--- examples/iterator.py
+++ examples/iterator.py
@@ -1,19 +1,19 @@
 """
 Example of a simple class implementing iterator
-protocol
+protocol (PEP 232)
 """
 
 
 class Countdown():
 
     def __init__(self, start):
-        self.counter = start
+        self.counter = start + 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        self.counter = self.counter - 1
         if self.counter < 1:
             raise StopIteration
-        self.counter -= 1
-        return self.counter + 1
+        return self.counter
